58915853d705f3bea0e8dde173464e499ad23896
Min96 <hoangminh135764@gmail.com>
2020-05-09 15:10:10 +0700

    Update NotificationPackage.java

--- android/src/main/java/io/neson/react/notification/NotificationPackage.java
+++ android/src/main/java/io/neson/react/notification/NotificationPackage.java
@@ -35,7 +35,6 @@ public class NotificationPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
